da3d4ee63c863e07026b429c065d91d2151c3933
Leland Husband <leland.husband@gmail.com>
2011-02-20 19:55:46 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hummod::Application.config.secret_token = '45dc0ebdea186a56b49e19ca8ea547ac6e1f402378ea233b9b9e6b201b07ebfc403018fe97941f0804d37b2ae0b0e2a7f0292b2cdd820f72c5f6e87a9fe22952'
