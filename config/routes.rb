b869937e886bd897480a45ca00418bc8e5011c6d
Leland Husband <leland.husband@gmail.com>
2011-02-21 11:58:10 -0600

    Started conversion of Erin's work to haml/sass.  I am eliminating some of the excess html too.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Hummod::Application.routes.draw do
   
   resources :downloads
+  
+  root :to => "downloads#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
