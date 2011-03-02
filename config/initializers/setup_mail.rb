34201c3c02ebe9cce438e74015f66462d9da29b3
Leland Husband <leland.husband@gmail.com>
2011-03-02 13:53:11 -0600

    removing setup_mail.rb

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :domain               => "hummod.com",  
-  :user_name            => "contact@hummod.com",  
-  :password             => "hummod",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
-}
